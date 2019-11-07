998258a71566bc2b16aaadc68bccf50f6bcc2fd6
lml <soalin228@163.com>
2019-11-07 21:31:17 +0800

    初始化

---
+++ code/557.js
@@ -0,0 +1,7 @@
+export default (str) => {
+  const arr = str.split(' ')
+  const result = arr.map(item => {
+    return item.split('').reverse().join('')
+  })
+  return result.join(' ')
+}
---
+++ test/557.test.js
@@ -0,0 +1,5 @@
+import revertByWorld from '../code/557'
+
+test('revertByWorld', () => {
+  expect(revertByWorld('Let\'s take LeetCode contest')).toBe('s\'teL ekat edoCteeL tsetnoc')
+})
